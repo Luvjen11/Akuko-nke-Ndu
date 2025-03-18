cc4852cb0f0a93cf860f50f2a5989f40e534f5d0
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-18 23:12:58 +0000

    structure Nav bar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {Link}  from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="navabr-container">
+      <Link to={"/"} className='logo-text'>Akụkọ nke Ndụ</Link>
+      <Link to={"/quotes"} className='nav-link'>View All</Link>
+    </nav>
+  );
 }
 
 export default Navbar
