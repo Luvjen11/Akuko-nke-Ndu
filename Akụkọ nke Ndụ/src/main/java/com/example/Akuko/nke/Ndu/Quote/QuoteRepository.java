13cc20fee779cf8a8bb9624f488c7c3331195558
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-18 16:41:09 +0000

    create Quote model and repository

---
+++ Akụkọ nke Ndụ/src/main/java/com/example/Akuko/nke/Ndu/Quote/QuoteRepository.java
@@ -0,0 +1,7 @@
+package com.example.Akuko.nke.Ndu.Quote;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuoteRepository extends JpaRepository<Quote, Long> {
+    
+}
